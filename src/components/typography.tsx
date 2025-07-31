240b12bf0c8362a9768ad66b8f14aab7279a8476
Wlad Meixner <wlad.meixner@bestbytes.com>
2025-07-31 11:44:14 +0200

    feat: add generic prose component

--- src/components/typography.tsx
+++ src/components/typography.tsx
@@ -1,1 +1,27 @@
-export const Prose = (props: React.PropsWithChildren<{ className?: string }>) => <div className={`prose ${props.className}`}>{props.children}</div>;
+import { Slot } from "@radix-ui/react-slot";
+import * as React from "react";
+
+interface ProseProps<T extends React.ElementType = "div"> {
+	asChild?: boolean;
+	as?: T;
+	className?: string;
+}
+
+const Prose = React.forwardRef<
+	HTMLDivElement,
+	ProseProps & React.ComponentPropsWithoutRef<"div">
+>(({ asChild = false, as, className, ...props }, ref) => {
+	if (asChild) {
+		return <Slot ref={ref} className={`prose ${className || ""}`} {...props} />;
+	}
+
+	const Component = as || "div";
+
+	return (
+		<Component ref={ref} className={`prose ${className || ""}`} {...props} />
+	);
+});
+
+Prose.displayName = "Prose";
+
+export { Prose, type ProseProps };
